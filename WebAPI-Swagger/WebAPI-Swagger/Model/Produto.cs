95a8f3360d12be41efbd6c8500cff0652696a349
Thiago Campos <thiago-kono@hotmail.com>
2024-05-13 21:03:53 -0300

    deleção de pastas criadas automaticamennte e Add Model

---
+++ WebAPI-Swagger/WebAPI-Swagger/Model/Produto.cs
@@ -0,0 +1,13 @@
+﻿namespace WebAPI_Swagger.Model
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public int Estoque { get; set; }
+    }
+}
--- WebAPI-Swagger/WebAPI-Swagger/obj/Debug/net7.0/WebAPI-Swagger.AssemblyInfo.cs
+++ WebAPI-Swagger/WebAPI-Swagger/obj/Debug/net7.0/WebAPI-Swagger.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI-Swagger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25c86fb09281f59e89acd57f6e11fef37a1aefeb")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI-Swagger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI-Swagger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
