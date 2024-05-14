3c60350f4870d016ada568793787e08884dffd68
Thiago Campos <thiago-kono@hotmail.com>
2024-05-13 22:20:48 -0300

    add Context, Pacotes nuget e AppDbContext

---
+++ WebAPI-Swagger/WebAPI-Swagger/Context/AppDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI_Swagger.Model;
+
+namespace WebAPI_Swagger.Context
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Produto>()
+                .Property(p => p.Nome)
+                .HasMaxLength(80);
+
+            modelBuilder.Entity<Produto>()
+                .Property(p => p.Preco)
+                .HasPrecision(10,2);
+
+
+            modelBuilder.Entity<Produto>()
+                .HasData(
+                new Produto { Id = 1, Nome = "Caderno", Preco = 7.99M, Estoque = 50 },
+                new Produto { Id = 2, Nome = "Lápis", Preco = 1.99M, Estoque = 100 },
+                new Produto { Id = 3, Nome = "Borracha", Preco = 0.75M, Estoque = 20 }
+                );
+
+        }
+
+    }
+}
--- WebAPI-Swagger/WebAPI-Swagger/obj/Debug/net7.0/WebAPI-Swagger.AssemblyInfo.cs
+++ WebAPI-Swagger/WebAPI-Swagger/obj/Debug/net7.0/WebAPI-Swagger.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI-Swagger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25c86fb09281f59e89acd57f6e11fef37a1aefeb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95a8f3360d12be41efbd6c8500cff0652696a349")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI-Swagger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI-Swagger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
