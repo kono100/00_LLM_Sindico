17ba4046f2ae93baa44e0c0a280aa37796972f95
Thiago Campos <thiago-kono@hotmail.com>
2024-06-08 17:43:20 -0300

    ...

--- WebAPI-Swagger/WebAPI-Swagger/obj/Debug/net7.0/WebAPI-Swagger.AssemblyInfo.cs
+++ WebAPI-Swagger/WebAPI-Swagger/obj/Debug/net7.0/WebAPI-Swagger.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI-Swagger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdad7ab3c4ec78e4bbe370959a2bcb840ed02419")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30759833a72d681d4e396515ad66182e14071d0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI-Swagger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI-Swagger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
